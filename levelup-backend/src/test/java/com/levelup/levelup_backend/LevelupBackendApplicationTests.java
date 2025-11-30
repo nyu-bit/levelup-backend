2a5a9feabc06ed319b1b26406f472a881a413860
María José Contreras González <mj.contrerasg@duocuc.cl>
2025-11-29 21:45:30 -0300

    Add files via upload
    estructura back

---
+++ levelup-backend/src/test/java/com/levelup/levelup_backend/LevelupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup.levelup_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
